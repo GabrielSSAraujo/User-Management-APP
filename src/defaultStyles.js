45e520ca54abefd84a8f8c2fb5b9b0686f886e7c
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-24 02:47:07 -0300

    pradronizando cores e fontes

---
+++ src/defaultStyles.js
@@ -0,0 +1,15 @@
+const fontSize = {
+    sizeSm:"12px",
+    sizeMd:"14px",
+    sizeLg:"18px",
+    sizeXLg:"24px",
+    sizeTitle:"36px",
+}
+const colors = {
+    white:"#ffffff",
+    black: "#000000",
+    brownBg:"#712817",
+    successAc:"#397A43",
+    buttonBg: "#E7B042",
+    inputColor: "#E5E5E5"
+}
