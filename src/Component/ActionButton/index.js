1d02cf9710668d32c828257be368dfb1b66b472e
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-24 03:24:47 -0300

    adicionado compoente para botao de acao

--- src/Component/ActionButton/index.js
+++ src/Component/ActionButton/index.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { StyledActionButton } from "./style";
 
 const ActionButton = () =>{
-    
+    return(
+        <StyledActionButton>
+            Entrar
+        </StyledActionButton>
+    )
 }
+
+export default ActionButton;
--- src/Component/ActionButton/style.js
+++ src/Component/ActionButton/style.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
+import { colors, fontSize } from "../../defaultStyles";
 
 const StyledActionButton = styled.button`
-    color:white;
-    background-color:#ccc;
+    color:${colors.brownBg};
+    font-weight: bold;
+    font-size: ${fontSize.sizeLg};
+    background-color: ${colors.buttonBg};
+    padding: 0.8rem 3.5rem;
+    border:none;
+    border-radius: 7px;
 `;
+
+export {StyledActionButton}
