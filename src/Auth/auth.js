6bf4b000ff4d2083bb8f5408cd60c32f61b8fc8a
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-24 20:23:11 -0300

    adicionando rotas privadas

---
+++ src/Auth/auth.js
@@ -0,0 +1,7 @@
+const TOKEN = "user_token";
+
+const login = (token) => localStorage.setItem(TOKEN, token);
+const logout = () => localStorage.removeItem(TOKEN);
+const isAuthenticated = () => !!localStorage.getItem(TOKEN);
+
+export {login, logout, isAuthenticated}
--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch,Redirect } from "react-router-dom";
 import LoginScreen from "./Pages/LoginScreen";
 import { history } from "./history";
+import {isAuthenticated} from '../src/Auth/auth'
+
+const PrivateRoutes = ({component:Component, ...args})=>(
+    <Route
+    {...args}
+    render = {(arg)=> isAuthenticated() ? (<Component {...args}/>):( 
+    <Redirect
+        to={{ pathname: "/login", state: { from: arg.location } }}
+      />)
+    }
+
+    />
+);
+
+
 
 const Routes = () =>(
     <BrowserRouter history={history}>
         <Switch>
-            <Route exact path="/" component={()=><h1>home</h1>}/>
-            <Route path="/login" component={()=> <LoginScreen/>}/>
+            <PrivateRoutes exact path="/" component={()=><h1>home</h1>}/>
+            <Route exact path="/login" component={()=> <LoginScreen/>}/>
         </Switch>
     </BrowserRouter>
 );
